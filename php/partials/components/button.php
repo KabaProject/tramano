e3caf5e34e102bc234f47a1f1d7dcadf6529ee2e
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-11-02 03:39:06 -0600

    button componente was created

---
+++ php/partials/components/button.php
@@ -0,0 +1,8 @@
+<a href="" class="button--icon button--accent button--rounded">
+	<span class="iconify button--icon__icon" data-icon="simple-icons:paypal" data-inline="false"></span>
+	<span class="button--icon__text"> Boton Guapo </span>
+</a>
+<button class="button--icon button--danger button--rounded">
+	<span class="iconify button--icon__icon" data-icon="grommet-icons:money" data-inline="false"></span>
+	<span class="button--icon__text"> Boton Guapo </span>
+</button>
