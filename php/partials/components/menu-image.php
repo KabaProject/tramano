1bf4cf112af6656b63fad70120aaf06b4fcec84d
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-11-03 11:22:47 -0600

    menu-image was created

---
+++ php/partials/components/menu-image.php
@@ -0,0 +1,6 @@
+<div class="menu-image">
+	<img src="src/img/donate-menu.svg" alt="" class="menu-image__image" style="width: 80%; bottom: 0; right: 20px;">
+	<p class="menu-image__title">
+		Donar al proyecto
+	</p>
+</div>
---
+++ php/partials/layouts/principal-nav-mobile.php
@@ -0,0 +1,8 @@
+<section class="principal-nav--mobile">
+	<a href="?page=home" class="principal-nav__brand">
+		Tramano
+	</a>
+	<button class="principal-nav__menu">
+		<span class="iconify principal-nav__icon" data-icon="ic:baseline-menu" data-inline="false"></span>
+	</button>
+</section>
