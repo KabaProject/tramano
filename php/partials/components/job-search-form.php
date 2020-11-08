31d4f68e4a6b507b7727c95765c49b86e712f5af
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-11-07 22:37:12 -0600

    job search form componente was created

---
+++ php/partials/components/job-search-form.php
@@ -0,0 +1,31 @@
+<form action="" class="job-search-form">
+	<div class="job-search-form__keyword">
+		<span class="iconify job-search-form__icon" data-icon="ic:outline-search" data-inline="false"></span>
+		<input type="text" class="job-search-form__input" placeholder="Puesto">
+	</div>
+	<div class="job-search-form__wrapper">
+	<div class="job-search-form__city">
+		<span class="iconify job-search-form__icon" data-icon="ic:outline-place" data-inline="false"></span>
+		<input type="text" class="job-search-form__input" placeholder="Ciudad">
+		<div class="job-search-form__options">
+			<p data-city="" class="job-search-form__option">
+				<span class="iconify job-search-form__icon--op" data-icon="ic:outline-place" data-inline="false"></span>
+				Ciudad de México
+			</p>
+			<p data-city="" class="job-search-form__option">
+				<span class="iconify job-search-form__icon--op" data-icon="ic:outline-place" data-inline="false"></span>
+				Monterrey
+			</p>
+			<p data-city="" class="job-search-form__option">
+				<span class="iconify job-search-form__icon--op" data-icon="ic:outline-place" data-inline="false"></span>
+				Guadalajara
+			</p>
+		</div>
+	</div>
+	<div class="job-search-form__button">
+		<button class="button button--accent button--rounded">
+			Buscar
+		</button>
+	</div>
+	</div>
+</form>
